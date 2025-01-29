a6863df60c455a000fe8e1f54d9d159fec10c8c1
Vladislav Petrosyan <petrosyanv@dynacare.ca>
2025-01-28 22:47:27 -0500

    added comment to end api

--- Chat_Backend/validators/index.js
+++ Chat_Backend/validators/index.js
@@ -1,6 +1,8 @@
 const { validationResult } = require("express-validator");
 
 exports.validate = (req, res, next) => {
+  // validationResult checks if previous
+  // validation rules are failed e.g. body("email").isEmail(),
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
