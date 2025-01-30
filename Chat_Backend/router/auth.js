f9279150d3e175fcb1a5f6b298204a5b5d00d969
Vladislav Petrosyan <petrosyanv@dynacare.ca>
2025-01-30 10:08:55 -0500

    minor fixes

--- Chat_Backend/router/auth.js
+++ Chat_Backend/router/auth.js
@@ -1,11 +1,16 @@
 const router = require("express").Router();
-const { login, register } = require("./../controllers/authController");
+
+const {
+  loginController,
+  registerController,
+} = require("./../controllers/authController");
+
 const { validate } = require("./../validators");
 const { rules: loginRules } = require("./../validators/auth/login");
 const { rules: registrationRules } = require("./../validators/auth/register");
 
-router.post("/login", [loginRules, validate], login);
+router.post("/login", [loginRules, validate], loginController);
 
-router.post("/register", [registrationRules, validate], register);
+router.post("/register", [registrationRules, validate], registerController);
 
 module.exports = router;
--- Chat_Backend/validators/index.js
+++ Chat_Backend/validators/index.js
@@ -1,8 +1,9 @@
 const { validationResult } = require("express-validator");
 
 exports.validate = (req, res, next) => {
-  // validationResult checks if previous validation
-  // rules are failed e.g. body("email").isEmail(),
+  // validationResult checks if previous
+  // validation rules are failed
+  // e.g. body("email").isEmail(),
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
