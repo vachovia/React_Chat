668a312c785a60fbe9322a78a9e8498e5768f265
Vladislav Petrosyan <petrosyanv@dynacare.ca>
2025-03-02 21:01:08 -0500

    chat controller: create, delete...

--- Chat_Backend/controllers/chatController.js
+++ Chat_Backend/controllers/chatController.js
@@ -32,5 +32,133 @@ exports.index = async (req, res) => {
     ],
   });
 
-  res.send(user.Chats);
+  res.json(user.Chats);
+};
+
+exports.create = async (req, res) => {
+  const { partnerId } = req.body;
+  const t = await models.sequelize.transaction();
+
+  try {
+    const user = await User.findOne({
+      where: {
+        id: req.user.id,
+      },
+      include: [
+        {
+          model: Chat,
+          where: {
+            type: "dual",
+          },
+          include: [
+            {
+              model: ChatUser,
+              where: {
+                userId: partnerId,
+              },
+            },
+          ],
+        },
+      ],
+    });
+
+    if (user && user.Chats.length > 0) {
+      return res
+        .status(403)
+        .json({ status: "Error", message: "Chat already exists" });
+    }
+
+    const chat = await Chat.create({ type: "dual" }, { transaction: t });
+
+    await ChatUser.bulkCreate(
+      [
+        {
+          chatId: chat.id,
+          userId: req.user.id,
+        },
+        {
+          chatId: chat.id,
+          userId: partnerId,
+        },
+      ],
+      { transaction: t }
+    );
+
+    await t.commit();
+
+    const chatNew = await Chat.findOne({
+      where: {
+        id: chat.id,
+      },
+      include: [
+        {
+          model: User,
+          where: {
+            [Op.not]: {
+              id: req.user.id,
+            },
+          },
+        },
+        {
+          model: Message,
+        },
+      ],
+    });
+
+    return res.json(chatNew);
+  } catch (e) {
+    console.log(e);
+    await t.rollback();
+    return res.status(500).json({ status: "Error", message: e.message });
+  }
+};
+
+exports.messages = async (req, res) => {
+  try {
+    const limit = 10;
+    const page = req.query.page || 1;
+    const offset = page > 1 ? page * limit : 0;
+
+    const messages = await Message.findAndCountAll({
+      where: {
+        chatId: req.query.id,
+      },
+      limit,
+      offset,
+    });
+
+    const totalPages = Math.ceil(messages.count / limit);
+
+    if (page > totalPages) {
+      return res.json({ data: { messages: [] } });
+    }
+
+    const result = {
+      messages: messages.rows,
+      pagination: {
+        page,
+        totalPages,
+      },
+    };
+
+    res.json(result);
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ status: "Error", message: e.message });
+  }
+};
+
+exports.deleteChat = async (req, res) => {
+  try {
+    await Chat.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });    
+
+    return res.json({ status: "Success", message: "Chat deleted" });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ status: "Error", message: e.message });
+  }
 };
--- Chat_Backend/router/chat.js
+++ Chat_Backend/router/chat.js
@@ -1,7 +1,10 @@
 const router = require("express").Router();
-const { index: chatController } = require("./../controllers/chatController");
+const { index, create, messages, deleteChat } = require("./../controllers/chatController");
 const { auth: authMiddleware } = require("./../middleware/auth");
 
-router.get("/", [authMiddleware], chatController);
+router.get("/", [authMiddleware], index);
+router.get("/messages", [authMiddleware], messages);
+router.post("/create", [authMiddleware], create);
+router.delete("/:id", [authMiddleware], deleteChat);
 
 module.exports = router;
